03ee910871981c0df19747b0b018d94face2ca96
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-08-09 18:32:02 +0300

    Created settings class with nested class inside

---
+++ HW4/HW4/ApplicationSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace HW4
+{
+	public class ApplicationSettings
+	{
+		public string Name { get; private set; } = "Homework Application";
+		public string Version { get; private set; } = "1.2";
+
+		public string GetApplicationInfo()
+		{
+			var temp = new DatabaseSettings();
+			return $"Application Name: {Name} \nVersion: {Version} \nDatabase info:" +
+				$" \nConnection string: {temp.ConnectionString}{temp.Timeout}";
+		}
+
+		private class DatabaseSettings
+		{
+			public string ConnectionString { get; private set; } = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;";
+			public string Timeout { get; private set; } = "Connect Timeout=30;";
+
+		}
+	}
+}
