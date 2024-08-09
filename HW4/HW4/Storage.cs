f2055e14189139bd1b1c16cc0422b20342d33638
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-08-09 19:02:56 +0300

    Created class Storage

---
+++ HW4/HW4/Storage.cs
@@ -0,0 +1,17 @@
+﻿namespace HW4
+{
+	public class Storage<T>
+	{
+		private static T? Instance { get; set; }
+
+		public void SetItem(T item)
+		{
+			Instance = item;
+		}
+
+		public T? GetItem()
+		{
+			return Instance;
+		}
+	}
+}
